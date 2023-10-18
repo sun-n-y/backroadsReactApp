9585e773f38abaf467ee9022d88590b556e1978f
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-18 13:26:26 -0400

    add components to app js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import About from './components/About';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Services from './components/Services';
+import Tours from './components/Tours';
+
 function App() {
   return (
     <>
-      <body></body>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
     </>
   );
 }
