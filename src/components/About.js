9c1f577c4c5d794b519caf10e68da00da6d7fa1e
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-18 15:47:27 -0400

    add footer links ans socials

--- src/components/About.js
+++ src/components/About.js
@@ -22,7 +22,7 @@ const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#" className="btn">
+          <a href="#home" className="btn">
             read more
           </a>
         </article>
